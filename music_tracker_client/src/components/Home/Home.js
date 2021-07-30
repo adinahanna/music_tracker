5281df314051f7d1695c4890f3cdc4458b331823
adinahanna <adina.weinberg@gmail.com>
2021-07-30 14:44:45 -0500

    complete delete functionality

---
+++ music_tracker_client/src/components/Home/Home.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+const Home = ({match}) => {
+    const [records, setRecords] = useState([]);
+    const history = useHistory();
+
+    const getRecordsData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/records/');
+            const data = await response.json();
+            setRecords(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const _handleDelete = async (id) => {
+        try {
+            const deletedRecord = await fetch(`http://localhost:8000/records/${id}`, {method: 'DELETE'});
+            if(deletedRecord.status === 204) {
+                getRecordsData();
+                history.push('/')
+            } else {
+                alert('Something went wrong. please try again.')
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getRecordsData();
+    }, []);
+
+
+    return (
+        <div>
+            <h1>Welcome to Record Tracker!</h1>
+            {/* create form */}
+            <h2>My Most Wanted Records</h2>
+            {records.map((record, idx) => {
+                return (
+                    <div key={idx} >
+                        <h3>{record.title}</h3>
+                        <h3>{record.artist}</h3>
+                        <button onClick={() => _handleDelete(record.id)}>Delete</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Home;
